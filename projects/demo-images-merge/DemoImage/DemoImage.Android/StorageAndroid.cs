32f4f91a828a1d55da49e4cf14bd9573430ba97d
Daniel Cantu Guajardo <e-dcantu@neoris.cxnetworks.net>
2017-09-22 11:21:32 -0500

    add 'DemoImage' xamarin.forms PCL => Merge images; dependency service android

---
+++ projects/demo-images-merge/DemoImage/DemoImage.Android/StorageAndroid.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Java.IO;
+using Java.Net;
+using Android.Graphics;
+using System.IO;
+
+[assembly: Dependency(typeof(DemoImage.Droid.StorageAndroid))]
+namespace DemoImage.Droid
+{
+    public class StorageAndroid : Storage.IImages
+    {
+        public string Location(string fileName)
+        {
+            string path = System.IO.Path.Combine(
+                Android.OS.Environment.ExternalStorageDirectory.AbsolutePath,
+                Android.OS.Environment.DirectoryDownloads, fileName);
+            return path;
+        }
+
+        public string MergeByUrl(string urlFront, string urlBack)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().PermitAll().Build();
+            StrictMode.SetThreadPolicy(policy);
+
+            Bitmap bitMapFront = GetImageByUrl(urlFront);
+            Bitmap bitMapBack = GetImageByUrl(urlBack);
+            if (bitMapBack == null || bitMapFront == null) return null;
+            Bitmap merged = Merge(bitMapFront, bitMapBack);
+            string fileName = "DemoImage.jpg";
+            string location = Location(fileName);
+            Save(location, merged);
+
+            return location;
+        }
+
+        /** private */
+        private Bitmap GetImageByUrl(string url)
+        {
+            try
+            {
+                HttpURLConnection connection = (HttpURLConnection)(new URL(url).OpenConnection());
+                connection.DoInput = true;
+                connection.Connect();
+                return BitmapFactory.DecodeStream(connection.InputStream);
+            }
+            catch (Exception e)
+            {
+                // Log exception
+                return null;
+            }
+        }
+
+        private Bitmap Merge(Bitmap front, Bitmap back)
+        {
+            Bitmap overlay = Bitmap.CreateBitmap(front.Width, front.Height, Bitmap.Config.Argb8888);
+            Canvas canvas = new Canvas(overlay);
+            // canvas.DrawColor(Android.Graphics.Color.Transparent, PorterDuff.Mode.Clear);
+            canvas.DrawBitmap(front, new Matrix(), null);
+            canvas.DrawBitmap(back, new Matrix(), null);
+            return overlay;
+        }
+
+        private void Save(string path, Bitmap bitmap)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                bitmap.Compress(Bitmap.CompressFormat.Jpeg, 100, fileStream);
+            }
+        }
+    }
+}
---
+++ projects/demo-images-merge/DemoImage/DemoImage/ViewModels/DemoImageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DemoImage
+{
+    class DemoImageViewModel : INotifyPropertyChanged
+    {
+        /** Binding */
+        public string ImgXamarin { protected set; get; }
+        public string ImgVisualStudio { protected set; get; }
+        public string ImgMerged { protected set; get; }
+        public ICommand MergeImagesCommand { protected set; get; }
+        /** end Binding */
+
+        // Contructor
+        public DemoImageViewModel()
+        {   
+            // Commandings
+            this.MergeImagesCommand = new Command(() => {
+                var platform = DependencyService.Get<Storage.IImages>();
+                if (platform == null) return;
+                var newSource = this.ImgMerged != null ? null : platform.MergeByUrl(this.ImgXamarin, this.ImgVisualStudio);
+                this.ImgMerged = newSource;
+                this.OnPropertyChanged("ImgMerged");
+            });
+
+            // Image Sources
+            this.ImgXamarin = "https://maxcdn.icons8.com/Share/icon/color/Logos/xamarin1600.png";
+            this.ImgVisualStudio = "http://icons.iconarchive.com/icons/dakirby309/simply-styled/128/Microsoft-Visual-Studio-icon.png";
+
+            // Notify Changes
+            this.OnPropertyChanged("ImgXamarin");
+            this.OnPropertyChanged("ImgVisualStudio");
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
